6425049cdbc7170584c57bd6993bf5dfc051e496
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-03-14 12:23:59 -0400

    Added secrets

---
+++ .github/scripts/decrypt_secret_files.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# The file names of the decrypted files, the encrypted file name contains the .gpg extension
+FILE_NAMES=('secret1.json', 'secret2.txt')
+
+for file_name in "${FILE_NAMES[@]}"
+do
+  gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_PASSPHRASE" \
+    --output "./$file_name" "$file_name.gpg"
+done
